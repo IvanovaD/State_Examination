6d7df6e2dc8636b008b1fd015bf3fe34d3899bc6
Diana Ivanova <didi.e.ivanova@gmail.com>
2020-09-04 19:15:42 +0300

    edited os

--- OS/OS_2019_07.c
+++ OS/OS_2019_07.c
@@ -13,6 +13,7 @@ abcdef
 f1
 ———————————————————————
 line
+x1
 
 
 
@@ -44,10 +45,10 @@ head -1 file //first line of the file
     display the names and values of shell variables.
  */
 grep `head -1 f1` `tail -1 f1` | wc -l > f2 //grep line line | wc -l > f2 
-//демек 0 линии, тъй като няма файл line 
-// *****************Ще изведе съобщение за грешка
-a=`cat f2` //a = 0
-echo $a $3 // ***********$a е 0, а $3 е третия позиционен параметър ef
+//3 линии
+// *****************
+a=`cat f2` //a = 3
+echo $a $3 // ***********$a е 3, а $3 е третия позиционен параметър ef
 set 9 7 5 3 //новите позиционни параметри са 9 7 5 3
 shift 2 // остават само 5 3 
 for j in 1 2 3 4 5 // повтаряме цикъла 5 пъти
@@ -56,15 +57,17 @@ do for i  //цикли по позиционните параметри
 			   // проверка дали   INTEGER1 is less than INTEGER2 и винаги е вярно
 	 
 		then cat f1 f2 > f3 //копирам от f1 и f2 във f3 и f3 става: line
-								//										0
-			wc -l f3			// *****броим редовете в f3 и те са 2
-								//тъй като цикъла се върти два пъти ще се повтори 2 пъти
+																    //x1
+								//									 3
+			wc -l f3			// *****броим редовете в f3 и те са 3 f3
+							
 			echo $i $j $a >> f4   // пишем във f4 
-									//            5 1 0
-									//            3 1 0
-else tee f2 f3 < f1 //пишем във f1 и едновременно изкарваме на станд изход
-			wc -w f2
-			echo $i $j $a >> f4
+									//            5 1 3
+									
+else tee f2 f3 < f1 //пишем от f1 и едновременно изкарваме на станд изход line 
+																	//    x1
+			wc -w f2    //2 f2
+			echo $i $j $a >> f4    // 3 1 3
 		fi
 	done //КРАЙ НА ВЪТРЕШНИЯ ЦИКЪЛ
 	echo $# >> f4 // брой аргументи позиционни  добавяме ред 2
@@ -74,16 +77,16 @@ read key1 key2 //четем от клавиатурата съответно 3 1
 while cat f4 | grep $key2 //****търсим в f4 1чката и я намираме 2 пъти
 	do sort f4 //SORT command sorts the contents of a text file, line by line
 						// ****
-						//	 2
-	                    //   3 1 0
-						//	 5 1 0
-	a=`wc -c < f4` //броя букви в f4 т.е 13
-	echo -n "Character count: $a" // 13
+						//	 2 
+	                    //   3 1 3
+						//	 5 1 3
+	a=`wc -c < f4` //броя букви в f4 т.е 1
+	echo -n "Character count: $a" // 14 = 11 + 3(new line)
 	exit  //излизаме от програмата
 
 				
 done                 // въобще не стигаме до тачи част
-grep $key1 f4     // връща реда с 3ка - 3 1 0
+grep $key1 f4     // връща реда с 3ка - 3 1 3
 b=`wc -l < f4`    // броу редове в f4 - 5
 echo -n "Lines count: $b" //5
 
@@ -93,14 +96,16 @@ echo -n "Lines count: $b" //5
 Output: 
 
 student@banshee:~$ bash comproc ab cd ef gh
-grep: line: No such file or directory
-0 ef
-2 f3
-2 f3
+
+3 ef
+3 f3
+line
+x1
+2 f2
 3 1
-5 1 0
-3 1 0
+5 1 3
+3 1 3
 2
-3 1 0
-5 1 0
+3 1 3
+5 1 3
 Character count: 14
